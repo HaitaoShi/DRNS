b1167300c74022050fc87c9e910dc78124a1949b
Haitao Shi <451483204@qq.com>
2024-02-15 10:51:35 +0000

    Remove redundant files
    Remove redundant files

--- app.R
+++ app.R
@@ -3,9 +3,11 @@ library(shiny)
 library(dplyr)
 library(plotly)
 library(DT)
+library(rsconnect)
 
-# Read the data
-data <- read.csv("Clean_data.csv", check.names = FALSE) 
+data <- read.csv("Clean_data.csv", check.names = FALSE) %>%
+    mutate(`Reference` = paste0("<a href='", `Links`, "' target='_blank'>", `Reference`, "</a>")) %>%
+    select(-Links) # 
 
 # user interface
 ui <- fluidPage(
@@ -21,7 +23,7 @@ ui <- fluidPage(
             .dataTable thead tr {
                 background-color: #f2f2f2; /* Light grey for column headers */
             }
- 
+
         "))
     ),
     sidebarLayout(
@@ -39,34 +41,41 @@ ui <- fluidPage(
 )
 
 
-# server function
 server <- function(input, output) {
     output$table <- renderDataTable({
-        if (input$name_input == "None" && input$institution_input == "None") {
-            data %>%
-            arrange(`Name of Researcher`, `Name of Institution`)
-        } else if (input$name_input == "None") {
-            subset(data, `Name of Institution` == input$institution_input) %>%
-            arrange(`Name of Researcher`)
-        } else if (input$institution_input == "None") {
-            subset(data, `Name of Researcher` == input$name_input) %>%
-            arrange(`Name of Institution`)
-        } else {
-            subset(data, `Name of Researcher` == input$name_input & `Name of Institution` == input$institution_input) %>%
-            arrange(`Name of Researcher`, `Name of Institution`)
+        # Apply filter conditions
+        filtered_data <- data
+        if (input$name_input != "None") {
+            filtered_data <- filtered_data %>%
+                filter(`Name of Researcher` == input$name_input)
         }
-    }, options = list(
-        pageLength = 5,  # Set default page length to 5
-        lengthMenu = list(c(5, 10, 25, 50, -1), c('5', '10', '25', '50', 'All'))  # Customize page length options
-    ))
-    
+        if (input$institution_input != "None") {
+            filtered_data <- filtered_data %>%
+                filter(`Name of Institution` == input$institution_input)
+        }
+
+        # Sort by name and institution if both are set to "None"
+        if (input$name_input == "None" && input$institution_input == "None") {
+            filtered_data <- filtered_data[order(tolower(filtered_data$`Name of Researcher`), tolower(filtered_data$`Name of Institution`)), ]
+        }
+
+        # Render data table
+        datatable(filtered_data, rownames = FALSE, options = list(
+            pageLength = 5,
+            lengthMenu = list(c(5, 10, 25, 50, -1), c("5", "10", "25", "50", "All"))
+        ), escape = FALSE) # Allow HTML content rendering
+    })
+
     output$pie_chart <- renderPlotly({
         institution_counts <- data %>% count(`Name of Institution`)
-        plot_ly(institution_counts, labels = ~`Name of Institution`, values = ~n, type = 'pie') %>%
-        layout(title = "Number of Researchers by Institution")
+        plot_ly(institution_counts, labels = ~`Name of Institution`, values = ~n, type = "pie") %>%
+            layout(title = "Number of Researchers by Institution")
     })
 }
 
 # Run the app
 shinyApp(ui = ui, server = server)
+
+# upload to shinyapps.io
+rsconnect::setAccountInfo(name = "lcf9k8-haitao0shi", token = "", secret = "")
 
